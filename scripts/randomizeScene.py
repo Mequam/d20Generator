92d5534da387fc14ae7b5d5b4ee90816db7ea211
mequam <blue9ja@gmail.com>
2023-06-24 21:58:54 -0400

    got the font changing randomly on the d20!

--- scripts/randomizeScene.py
+++ scripts/randomizeScene.py
@@ -4,7 +4,8 @@ scene containing the d20 for ai generation
 """
 import os 
 import bpy 
-import random
+import random 
+import time
 
 #walks through the file structure and loads all of the fonts at the given path
 #WARNING: this is a recursive walk, so gaurd your sym links and large folders
@@ -18,6 +19,9 @@ def load_fonts(path):
 #example accessing the fonts print([f for f in bpy.data.fonts])
 #randomizes the font on the d20
 def randomize_font():
+    uv_texture_path = f"./uvTextureBuffer.png"
+    normal_texture_path = f"./normalTextureBuffer.png"
+
     bpy.context.window.scene = bpy.data.scenes['geoNodeTexture']
 
     obj = bpy.context.scene.objects["Cube.001"]
@@ -28,18 +32,52 @@ def randomize_font():
 
     #render out an image without using the compistor for blur
     bpy.context.scene.use_nodes = False
-    bpy.context.scene.render.filepath="./test_d20_texture.png"
+    bpy.context.scene.render.filepath=uv_texture_path
     bpy.ops.render.render(write_still = True)
 
     #render out an image  using the compistor for blur
     bpy.context.scene.use_nodes = True
-    bpy.context.scene.render.filepath="./bump_blur.png"
+    bpy.context.scene.render.filepath=normal_texture_path
     bpy.ops.render.render(write_still = True)
 
     bpy.context.window.scene = bpy.data.scenes['d20']
    
-    
+    #load and set the uv texture for the dice to randomize the font
+    #open the files that we outputed
+    #bpy.ops.image.open(filepath="./",
+    #    directory="./",
+    #    files=[{"name":"bb.png",
+    #    "name":"d20.png"}],
+    #    relative_path=True 
+    #    )    
+    if 'uvTextureBuffer.png' in bpy.data.images:
+        print('[*] removing previous texture image!')
+        bpy.data.images.remove(bpy.data.images['uvTextureBuffer.png'])
+    if 'normalTextureBuffer.png' in bpy.data.images:
+        print('[*] removing previous normal image!')
+        bpy.data.images.remove(bpy.data.images['normalTextureBuffer.png'])
+
+    print("after open")
+    uv_texture = bpy.data.images.load(uv_texture_path,check_existing=True)
+    #bpy.data.images["uvTexture.png"]
+    normal_texture = bpy.data.images.load(normal_texture_path,check_existing=True)
+    #bpy.data.images["normalTexture.png"]
+
+    #tell the material of the dice to use those images
+
+    dice = bpy.context.scene.objects["rotater"]
+    shader_material = dice.material_slots[0].material 
+
+    shader_material.node_tree.nodes['Image Texture'].image = uv_texture
+    shader_material.node_tree.nodes['Image Texture.003'].image = normal_texture 
 
 
 
-randomize_font()
+#use a main function,
+#like some c++ NERD
+def main():
+    randomize_font()
+
+
+if __name__ == '__main__':
+    main()
